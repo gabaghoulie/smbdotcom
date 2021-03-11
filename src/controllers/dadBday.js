32de7c903e67f5613bc0e8a4ec3fa17309185923
Casey Burns <caseyburns33@gmail.com>
2021-03-10 17:32:39 -0800

    final dad bday

--- src/controllers/dadBday.js
+++ src/controllers/dadBday.js
@@ -10,6 +10,7 @@ var speed;
 var stars;
 
 var img;
+var img_idx;
 
 // Text overlay info + offsets
 // let name_txt = "";
@@ -34,7 +35,7 @@ var img;
 
 function preload() {
   // myFont = loadFont('./src/resources/cqmono.otf');
-  img = loadImage('../src/resources/1-hooters.png');
+  img = loadImage('https://i.imgur.com/CZwuAoI.png');
 }
 
 
@@ -44,7 +45,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 
   num_stars = 444;
-  speed = 5;
+  speed = 2;
   stars = [];
 
   noStroke();
@@ -52,6 +53,8 @@ function setup() {
   for (let i = 0; i < num_stars; i++) {
     stars.push(new Star());
   }
+
+  img_idx = 0;
 }
 
 function draw() {
@@ -66,10 +69,70 @@ function draw() {
     stars[i].show();
   }
 
-  image(img, width * 0.1, height * 0.05);
-
+  // hooters
+  if (img_idx == 0) {
+    image(img, 0 - (width * 0.43) , 0 - (height * 0.45), width * 0.85, height * 0.85);
+  }
+  // matt 
+  if (img_idx == 1) {
+    image(img, 0 - (width * 0.43) , 0 - (height * 0.45), width * 0.85, height * 0.85);
+  }
+  // fam vegas
+  if (img_idx == 2) {
+    image(img, 0 - (width * 0.43) , 0 - (height * 0.45), width * 0.85, height * 0.85);
+  }
+  // fam cliffs
+  if (img_idx == 3) {
+    image(img, 0 - (width * 0.43) , 0 - (height * 0.45), width * 0.85, height * 0.85);
+  }
+  // bob
+  if (img_idx == 4) {
+    image(img, 0 - (width * 0.43) , 0 - (height * 0.45), width * 0.85, height * 0.85);
+  }
+  // xmas
+  if (img_idx == 5) {
+    image(img, 0 - (width * 0.43) , 0 - (height * 0.45), width * 0.85, height * 0.85);
+  }
+  // txt1
+  if (img_idx >= 6) {
+    image(img, 0 - (width * 0.43) , 0 - (height * 0.45), width * 0.85, height * 0.85);
+  }
 }
 
 function windowResized() {
    resizeCanvas(windowWidth, windowHeight);
+}
+
+function mouseClicked() {
+  img_idx += 1;
+  if (img_idx == 1) {
+    img = loadImage('https://i.imgur.com/8szA5Ia.png');
+  }
+  else if (img_idx == 2) {
+    img = loadImage('https://i.imgur.com/1Cl9e7F.jpg');
+  }
+  else if (img_idx == 3) {
+    img = loadImage('https://i.imgur.com/uPTEk2E.jpg');
+  }
+  else if (img_idx == 4) {
+    img = loadImage('https://i.imgur.com/t1aV8g4.png');
+  }
+  else if (img_idx == 5) {
+    img = loadImage('https://i.imgur.com/eyKwxGS.jpg');
+  }
+  else if (img_idx == 6) {
+    img = loadImage('https://i.imgur.com/sPzdssC.png');
+  }
+  else if (img_idx == 7) {
+    img = loadImage('https://i.imgur.com/XutGI9y.png');
+  }
+  else if (img_idx == 8) {
+    img = loadImage('https://i.imgur.com/z7GXi40.png');
+  }
+  else if (img_idx == 9) {
+    img = loadImage('https://i.imgur.com/0ictp7l.png');
+  }
+  else if (img_idx > 9) {
+    window.location.href = 'https://youtu.be/b8tPX7Km4P8';
+  }
 }
